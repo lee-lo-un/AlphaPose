1f687d6e361ba50c8c08474a51fee65f6d0a9ef7
Yeongjin Yu <124529023+yjyuwisely@users.noreply.github.com>
2024-11-21 16:34:19 +0900

    Resolve merge conflicts

--- LangChain/__init__.py
+++ LangChain/__init__.py
@@ -6,3 +6,5 @@ from langgraph.graph.message import add_messages
 class State(TypedDict):
     # 메시지 정의(list type 이며 add_messages 함수를 사용하여 메시지를 추가)
     messages: Annotated[list, add_messages]
+
+     
